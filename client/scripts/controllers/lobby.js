be49cbdb8f5f76023bb637e4226f17195cf174fc
Nathan Self <nathan@theselfs.org>
2016-01-31 19:22:41 -0500

    Show gameName as disabled input on lobby page

--- client/scripts/controllers/lobby.js
+++ client/scripts/controllers/lobby.js
@@ -2,6 +2,7 @@ angular.module('spoons')
   .controller('LobbyCtrl', function ($scope, $location, GameService) {
     console.log('set game name to:?', $location.path().substring(1));
     GameService.gameName($location.path().substring(1));
+    $scope.gameName = GameService.gameName();
     $scope.sendPlayerName = function () {
       GameService.setPlayerName($scope.playerName);
       $location.path(GameService.gameName() + '/avatars');
