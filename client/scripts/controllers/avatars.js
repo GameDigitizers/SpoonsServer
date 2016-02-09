595a3ce3ce4327d88b75b12d30e9c55f1f1d7af4
Michael Stewart <tgm@vt.edu>
2016-02-08 23:51:40 -0500

    begins to address #28 could eventually show a cursor for all characters in the selection process moving around? still doesn't help with #32

--- client/scripts/controllers/avatars.js
+++ client/scripts/controllers/avatars.js
@@ -4,54 +4,76 @@ angular.module('spoons')
       console.log("User picked", slide);
     };
 
+    $scope.selectFromList = function(slideIndex) {
+      console.log('selectFromList', slideIndex);
+      $scope.slides[slideIndex].active = true;
+      console.log($slides);
+    };
+
     $scope.slides = [
       {
         image: "/images/beaver.png",
+        active: true,
       },
       {
         image: "/images/bee.png",
+        active: false,
       },
       {
         image: "/images/chicken.png",
+        active: false,
       },
       {
         image: "/images/cow.png",
+        active: false,
       },
       {
         image: "/images/dog.png",
+        active: false,
       },
       {
         image: "/images/elephant.png",
+        active: false,
       },
       {
         image: "/images/giraffe.png",
+        active: false,
       },
       {
         image: "/images/goat.png",
+        active: false,
       },
       {
         image: "/images/hippo.png",
+        active: false,
       },
       {
         image: "/images/bear.png",
+        active: false,
       },
       {
         image: "/images/owl.png",
+        active: false,
       },
       {
         image: "/images/penguin.png",
+        active: false,
       },
       {
         image: "/images/pig.png",
+        active: false,
       },
       {
         image: "/images/sheep.png",
+        active: false,
       },
       {
         image: "/images/turkey.png",
+        active: false,
       },
       {
         image: "/images/zebra.png",
+        active: false,
       },
     ];
 });
