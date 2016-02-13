dd0145e56bbd54e0306938773f7d9f4d140a9f95
Michael Stewart <tgm@vt.edu>
2016-02-13 14:21:06 -0500

    animate show and hide

--- client/scripts/controllers/main.js
+++ client/scripts/controllers/main.js
@@ -1,7 +1,10 @@
 angular.module('spoons')
   .controller('MainCtrl', function ($scope, $location, GameService) {
+    $scope.gameNameSubmitted = false;
     $scope.go = function () {
+      $scope.gameNameSubmitted = true;
       $location.path($scope.gameName);
+
       // $window.location.href = '/'+$scope.gameName;
     };
   });
